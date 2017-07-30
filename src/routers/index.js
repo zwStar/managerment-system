ce7339c4112e4671556292abd3761067f7f54c53
fengyong <373209197@qq.com>
2017-07-30 20:14:07 +0800

    for pull

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,6 +5,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../view/Login/index.vue'
 import Layout from '../view/Layout/index.vue'
+import teacherList from '../view/Teacher/teacherList.vue'
+
 
 Vue.use(Router);
 const routes = [
@@ -12,11 +14,15 @@ const routes = [
         path: "/",
         component: Layout,
         children: [
-
+            {
+                path:"/teacherList",
+                component:teacherList
+            }
         ]
     },
     {
-        path: "/login", component: Login
+        path: "/login",
+        component: Login
     }
 ]
 
---
+++ src/view/Teacher/teacherList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="teacherList">
+        <el-input icon="search" placeholder="请输入教师工号"></el-input>
+        <el-button icon="plus" @click="addTeacherForm = true">新增教师</el-button>
+        <el-dialog title="新增教师" :visible.sync="addTeacherForm">
+            <el-form :model="form">
+
+            </el-form>
+        </el-dialog>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            addTeacherForm:false,
+        }
+    }
+}
+</script>
+<style lang="scss" rel="stylesheet/scss">
+
+</style>
