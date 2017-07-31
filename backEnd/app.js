09041de0b4713a663534253a84fa08b2c9d8e334
fengyong <373209197@qq.com>
2017-07-31 17:13:59 +0800

    aa

---
+++ backEnd/app.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Administrator on 2017/7/31.
+ */
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+//跨域
+app.all('http://127.0.0.1:3000', function (req, res, next) {
+    res.header("Access-Control-Allow-Credentials", true)
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+    res.header("X-Powered-By", ' 3.2.1')
+    res.header("Content-Type", "application/x-www-form-urlencoded")
+    next()
+})
+
+app.use("/addTeacher",require("./router/addTeacher"));
+
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`server running @${port} `);
+})
---
+++ backEnd/router/addTeacher.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/7/31.
+ */
+var express = require("express");
+var router = express.Router();
+
+router.post("/",function (req,res) {
+    
+});
